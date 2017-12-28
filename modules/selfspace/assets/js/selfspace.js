6878034dc57c05bcb8d23d0e8724a082d31fbee9
freshzxf <370234987@qq.com>
2017-12-28 16:44:20 +0800

    ..

---
+++ modules/selfspace/assets/js/selfspace.js
@@ -0,0 +1,11 @@
+/**
+ news入口
+ **/
+layui.define(['layer', 'flow','element','carousel','util'], function(exports){
+    var layer = layui.layer,
+        element = layui.element ,
+        flow = layui.flow,
+        carousel = layui.carousel,
+        util = layui.util;
+    exports('selfspace', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
+});
